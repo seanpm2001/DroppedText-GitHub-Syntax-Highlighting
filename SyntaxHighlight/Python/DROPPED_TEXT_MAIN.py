7273f4272b39b34db5bf4493f601faae3a9841f6
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-02-20 15:34:16 -0800

    DROPPED_TEXT_MAIN.py [V1]
    Version 1 of the dropped text syntax highlighting main script.

---
+++ SyntaxHighlight/Python/DROPPED_TEXT_MAIN.py
@@ -0,0 +1,79 @@
+# Start of script
+'''
+DROPPED_TEXT_MAIN.py
+The dropped Text syntax highlighting engine, written in Python 3.8
+Written by @seanpm2001
+I don't entirely know how to do this, so this is just pseudocode and planningfor now.
+'''
+# Import section
+import os
+'''import color'''
+# Functions
+''' Files we are working withb here:
+syntax.comment-default.drotex
+syntax.configline.drotex
+syntax.encoding.drotex
+syntax.osdefine_br.drotex
+syntax.languagespec-def.drotex
+syntax.filedate-def.drotex
+syntax-filecontainer-def.drotex
+'''
+_syntax_comment_default():
+  # Method coming soon
+  f = open("/SyntaxHighlight/DroppedText/drotex/syntax.comment-default.drotex", "a")
+  f.write("")
+  f.close()
+  break
+_syntax_configline_default():
+  # Method coming soon
+  f = open("/SyntaxHighlight/DroppedText/drotex/syntax.configline.drotex", "a")
+  f.write("")
+  f.close()
+  break
+_syntax_encoding_default():
+  # Method coming soon
+  f = open("/SyntaxHighlight/DroppedText/drotex/syntax.encoding.drotex", "a")
+  f.write("")
+  f.close()
+  break
+_syntax_osdefine_default():
+  # Method coming soon
+  f = open("/SyntaxHighlight/DroppedText/drotex/syntax.osdefine_br.drotex", "a")
+  f.write("")
+  f.close()
+  break
+_syntax_languagespec_default():
+  # Method coming soon
+  f = open("/SyntaxHighlight/DroppedText/drotex/syntax.languagespec-def.drotex", "a")
+  f.write("")
+  f.close()
+  break
+_syntax_fileDate_default():
+  # Method coming soon
+  f = open("/SyntaxHighlight/DroppedText/drotex/syntax.filedate-def.drotex", "a")
+  f.write("")
+  f.close()
+  break
+_syntax_filecontainerformat_default():
+  # Method coming soon
+  f = open("/SyntaxHighlight/DroppedText/drotex/syntax-filecontainer-def.drotex", "a")
+  f.write("")
+  f.close()
+  break
+# Main/Start
+print("Generating syntax highlight stylesheets for DroppedText")
+return _syntax_comment_default()
+return _syntax_configline_default()
+return _syntax_encoding_default()
+return _syntax_osdefine_default()
+return _syntax_languagespec_default()
+return _syntax_fileDate_default()
+return _syntax_filecontainerformat_default()
+break
+# End
+""" File info
+File type: Python 3.8 source file (*.py)
+File version: 1 (2022, Sunday, February 20th at 3:34 pm)
+Line count (including blank lines and compiler line): 80
+"""
+# End of script
